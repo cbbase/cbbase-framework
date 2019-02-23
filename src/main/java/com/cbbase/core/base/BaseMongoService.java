4fb73942a09fab1a93fd29c49852f9dc08735836
changbo <changbo@dgg.net>
2019-02-23 10:39:35 +0800

    删除多余的

--- src/main/java/com/cbbase/core/base/BaseMongoService.java
+++ src/main/java/com/cbbase/core/base/BaseMongoService.java
@@ -1,6 +1,5 @@
 package com.cbbase.core.base;
 
-import java.util.Collection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,40 +15,11 @@ public class BaseMongoService {
     @Autowired
     protected MongoTemplate mongoTemplate;
 
-    public < T > T findById(Long id, Class < T > entityClass) {
-        return mongoTemplate.findById(id, entityClass);
+    public PageContainer selectPage(Class < ? > entityClass, PageContainer param, Query query) {
+        return selectPage(entityClass, param, query, null);
     }
 
-    public < T > List < T > find(Query query, Class < T > entityClass) {
-        return mongoTemplate.find(query, entityClass);
-    }
-
-    public void insert(Object entity) {
-        mongoTemplate.insert(entity);
-    }
-
-    public void insertAll(Collection < ? > objectsToSave) {
-        mongoTemplate.insertAll(objectsToSave);
-        ;
-    }
-
-    public void update(Object entity) {
-        mongoTemplate.save(entity);
-    }
-
-    public void delete(Object entity) {
-        mongoTemplate.remove(entity);
-    }
-
-    public void deleteAll(Query query, Class < ? > entityClass) {
-        mongoTemplate.findAllAndRemove(query, entityClass);
-    }
-
-    public PageContainer selectPage(Class < ? > entityClass, Query query, PageContainer param) {
-        return selectPage(entityClass, query, param, null);
-    }
-
-    public PageContainer selectPage(Class < ? > entityClass, Query query, PageContainer param, Sort sort) {
+    public PageContainer selectPage(Class < ? > entityClass, PageContainer param, Query query, Sort sort) {
         if (query == null) {
             query = new Query();
         }
