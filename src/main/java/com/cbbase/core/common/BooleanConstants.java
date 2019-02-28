24e3ac7a87b028820f2ce9bd06ce61914b7771dd
changbo <changbo@dgg.net>
2019-02-28 09:57:17 +0800

    新增常量

---
+++ src/main/java/com/cbbase/core/common/BooleanConstants.java
@@ -0,0 +1,20 @@
+package com.cbbase.core.common;
+
+/**
+ * 	是否常量
+ * @author changbo
+ *
+ */
+public class BooleanConstants {
+
+	/**
+	 * 	是
+	 */
+	public static final int YES = 1;
+	
+	/**
+	 * 	否
+	 */
+	public static final int NO = 0;
+
+}
