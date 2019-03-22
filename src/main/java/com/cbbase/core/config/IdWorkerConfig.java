ac1dfd2007600f1c3df7dffd04d74361a4e2e376
changbo <changbo@dgg.net>
2019-03-22 16:59:00 +0800

    修改id参数注入方式

--- src/main/java/com/cbbase/core/config/IdWorkerConfig.java
+++ src/main/java/com/cbbase/core/config/IdWorkerConfig.java
@@ -1,18 +1,22 @@
 package com.cbbase.core.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.cbbase.core.common.ApplicationConfig;
 import com.cbbase.core.tools.IdWorker;
 
 @Configuration
 public class IdWorkerConfig {
 	
+	@Value("${idworker.workerId}")
+	private int workerId;
+	
+	@Value("${idworker.dataCenterId}")
+	private int dataCenterId;
+	
     @Bean
     public IdWorker idWorker() {
-    	long workerId = ApplicationConfig.getLong("idworker.workerId");
-    	long dataCenterId = ApplicationConfig.getLong("idworker.dataCenterId");
         return new IdWorker(workerId, dataCenterId);
     }
     
