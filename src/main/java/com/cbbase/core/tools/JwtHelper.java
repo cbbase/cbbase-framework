42d392b1a7a2419fee914cfde8075fb96eb095e0
changbo <changbo@dgg.net>
2019-04-16 13:48:50 +0800

    jwt修改

--- src/main/java/com/cbbase/core/tools/JwtHelper.java
+++ src/main/java/com/cbbase/core/tools/JwtHelper.java
@@ -7,6 +7,7 @@ import javax.crypto.spec.SecretKeySpec;
 import org.apache.commons.codec.binary.Base64;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -32,15 +33,12 @@ public class JwtHelper {
     
     public static <T> T parse(String key, String jwt, Class<T> clazz) {
         byte[] encodedKey = Base64.decodeBase64(key);
-        Claims claims = Jwts.parser().setSigningKey(encodedKey).parseClaimsJws(jwt).getBody();
-        return claims.get("data", clazz);
+        try {
+            Claims claims = Jwts.parser().setSigningKey(encodedKey).parseClaimsJws(jwt).getBody();
+            return claims.get("data", clazz);
+		} catch (ExpiredJwtException e) {
+			return null;
+		}
     }
-    
-    public static void main(String[] args) {
-		String token = JwtHelper.create("key", "123123", 6000);
-		String data = JwtHelper.parse("key", token, String.class);
-		System.out.println(token);
-		System.out.println(data);
-	}
 
 }
