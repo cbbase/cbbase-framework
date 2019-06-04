9e19e9ab3ccc7b98ae2058b712a774ca58c31b81
changbo <changbo>
2019-06-04 17:25:19 +0800

    新增线程缓存清除

---
+++ src/main/java/com/cbbase/core/interceptor/CacheClearInterceptor.java
@@ -0,0 +1,28 @@
+package com.cbbase.core.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.cbbase.core.cache.ThreadCache;
+
+/**
+ *	清除线程缓存
+ * @author changbo
+ *
+ */
+public class CacheClearInterceptor extends HandlerInterceptorAdapter {
+
+	protected Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		super.afterCompletion(request, response, handler, ex);
+		ThreadCache.getCacheMap().clear();
+	}
+	
+}
