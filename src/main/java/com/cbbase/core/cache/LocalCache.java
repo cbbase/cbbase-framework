a2b2ba13934614c8e6d66042ae16831239d6ea15
changbo <changbo>
2019-07-01 13:47:04 +0800

    1

--- src/main/java/com/cbbase/core/cache/LocalCache.java
+++ src/main/java/com/cbbase/core/cache/LocalCache.java
@@ -1,11 +1,7 @@
 package com.cbbase.core.cache;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-
-import com.cbbase.core.tools.JsonUtil;
-import com.cbbase.core.tools.StringUtil;
 
 
 /**
@@ -15,62 +11,16 @@ import com.cbbase.core.tools.StringUtil;
  */
 public class LocalCache {
 	
-	private static Map<String, String> map = new ConcurrentHashMap<>();
+	private static Map<String, Object> map = new ConcurrentHashMap<>();
 	
-	public static void put(String key, String value) {
+	public static void put(String key, Object value) {
 		map.put(key, value);
 	}
 	
-	public static String get(String key) {
-		return map.get(key);
+	@SuppressWarnings("unchecked")
+	public static <T> T get(String key){
+		return (T) map.get(key);
 	}
 	
-	public static int getAsInt(String key) {
-		String value = map.get(key);
-		if(StringUtil.isEmpty(value)) {
-			return 0;
-		}
-		try {
-			return Integer.valueOf(map.get(value));
-		}catch (Exception e) {
-			return 0;
-		}
-	}
-	
-	public static long getAsLong(String key) {
-		String value = map.get(key);
-		if(StringUtil.isEmpty(value)) {
-			return 0;
-		}
-		try {
-			return Long.valueOf(map.get(value));
-		}catch (Exception e) {
-			return 0;
-		}
-	}
-	
-	public static Map<String, String> getAsMap(String key) {
-		String value = map.get(key);
-		if(StringUtil.isEmpty(value)) {
-			return new HashMap<>();
-		}
-		try {
-			return JsonUtil.toMap(value);
-		}catch (Exception e) {
-			return new HashMap<>();
-		}
-	}
-	
-	public static <T> T getAsObject(String key, Class<T> clazz) {
-		String value = map.get(key);
-		if(StringUtil.isEmpty(value)) {
-			return null;
-		}
-		try {
-			return JsonUtil.toObject(value, clazz);
-		}catch (Exception e) {
-			return null;
-		}
-	}
 
 }
